ae2fe9dbcc0d66e779716529538ad93e3213dd9e
racine fellipe <racine.3301@gmail.com>
2023-06-26 16:43:22 -0300

    criando componente para cadastro de itens

--- src/componentes/cadastro/index.jsx
+++ src/componentes/cadastro/index.jsx
@@ -1,8 +1,46 @@
-function Cadastro() {
+import React from "react";
+import { Table } from "react-bootstrap";
+
+class Cadastro extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      produtos: [
+        { id: 1, nome: "Item-01", link: "www.item-01.com" },
+        { id: 2, nome: "Item-02", link: "www.item-02.com" },
+        { id: 3, nome: "Item-03", link: "www.item-03.com" },
+        { id:43, nome: "Item-04", link: "www.item-04.com" },
+      ],
+    };
+  }
+  render() {
     return (
-        <div>
-            <p>Página Cadastro</p>
-        </div>
-    )
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Link</th>
+            <th>Opções</th>
+          </tr>
+        </thead>
+        <tbody>
+
+          {
+            this.state.produtos.map((produtos) => 
+
+                <tr key={produtos.toString()}>
+
+                    <td>{produtos.nome}</td>
+                    <td>{produtos.link}</td>
+                    <td>Atualizae Excluir</td>
+
+                </tr>
+            )
+          }
+
+        </tbody>
+      </Table>
+    );
+  }
 }
 export default Cadastro;
